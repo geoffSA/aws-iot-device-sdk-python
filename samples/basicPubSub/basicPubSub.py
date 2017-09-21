fd9efdd44f29be8446c6fa220e3ac97feb4a6b67
Geoff <geoffrey.choy@secureagility.com>
2017-09-21 11:58:18 +1000

    Modified basic subscribe/publish example so topic names are different.

--- samples/basicPubSub/basicPubSub.py
+++ samples/basicPubSub/basicPubSub.py
@@ -29,70 +29,83 @@ def customCallback(client, userdata, message):
     print(message.topic)
     print("--------------\n\n")
 
+def publishLoop():
+    # Publish to the same topic in a loop forever
+    loopCount = 0
+    while True:
+        print('Publishing message')
+        print("--------------\n\n")
+        myAWSIoTMQTTClient.publish(topic, "New Message to AWS IoT: " + str(loopCount), 1)
+        loopCount += 1
+        time.sleep(2)
+    
+    
+if __name__ == "__main__":
+    # Read in command-line parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host",
+                        help="Your AWS IoT custom endpoint")
+    parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath",
+                        help="Root CA file path")
+    parser.add_argument("-c", "--cert", action="store", dest="certificatePath",
+                        help="Certificate file path")
+    parser.add_argument("-k", "--key", action="store", dest="privateKeyPath",
+                        help="Private key file path")
+    parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                        help="Use MQTT over WebSocket")
+    parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
+                        help="Targeted client id")
+    parser.add_argument("-t", "--topic", action="store", dest="topic",
+                        default="sdk/test/Python", help="Targeted topic")
 
-# Read in command-line parameters
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
-parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
-parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
-parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
-parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
-                    help="Use MQTT over WebSocket")
-parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
-                    help="Targeted client id")
-parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
+    args = parser.parse_args()
+    host = args.host
+    rootCAPath = args.rootCAPath
+    certificatePath = args.certificatePath
+    privateKeyPath = args.privateKeyPath
+    useWebsocket = args.useWebsocket
+    clientId = args.clientId
+    topic = args.topic
+    
+    if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+        parser.error("X.509 cert authentication and WebSocket are mutual exclusive. \
+        Please pick one.")
+        exit(2)
+        
+    if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+        parser.error("Missing credentials for authentication.")
+        exit(2)
 
-args = parser.parse_args()
-host = args.host
-rootCAPath = args.rootCAPath
-certificatePath = args.certificatePath
-privateKeyPath = args.privateKeyPath
-useWebsocket = args.useWebsocket
-clientId = args.clientId
-topic = args.topic
+    # Configure logging
+    logger = logging.getLogger("AWSIoTPythonSDK.core")
+    #logger.setLevel(logging.DEBUG)
+    streamHandler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    streamHandler.setFormatter(formatter)
+    #logger.addHandler(streamHandler)
 
-if args.useWebsocket and args.certificatePath and args.privateKeyPath:
-    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
-    exit(2)
+    # Init AWSIoTMQTTClient
+    myAWSIoTMQTTClient = None
+    if useWebsocket:
+        myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+        myAWSIoTMQTTClient.configureEndpoint(host, 443)
+        myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+    else:
+        myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+        myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+        myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
 
-if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
-    parser.error("Missing credentials for authentication.")
-    exit(2)
+    # AWSIoTMQTTClient connection configuration
+    myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+    myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+    myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-# Configure logging
-logger = logging.getLogger("AWSIoTPythonSDK.core")
-logger.setLevel(logging.DEBUG)
-streamHandler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-streamHandler.setFormatter(formatter)
-logger.addHandler(streamHandler)
+    # Connect and subscribe to AWS IoT
+    myAWSIoTMQTTClient.connect()
+    myAWSIoTMQTTClient.subscribe("sdk/fromaws/Python", 1, customCallback)
+    print('Connected to Client')
+    time.sleep(2)
 
-# Init AWSIoTMQTTClient
-myAWSIoTMQTTClient = None
-if useWebsocket:
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
-    myAWSIoTMQTTClient.configureEndpoint(host, 443)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
-else:
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-    myAWSIoTMQTTClient.configureEndpoint(host, 8883)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
-
-# AWSIoTMQTTClient connection configuration
-myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-
-# Connect and subscribe to AWS IoT
-myAWSIoTMQTTClient.connect()
-myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
-time.sleep(2)
-
-# Publish to the same topic in a loop forever
-loopCount = 0
-while True:
-    myAWSIoTMQTTClient.publish(topic, "New Message " + str(loopCount), 1)
-    loopCount += 1
-    time.sleep(1)
+    publishLoop()
